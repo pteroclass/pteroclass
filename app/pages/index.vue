ef17347099d2797f01e6a9f37207b4885c6de6f3
lorypelli <lorypelli@proton.me>
2026-01-11 10:06:58 +0100

    removed better auth in favor of manual authentication

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,7 @@
-<template>
-    <UForm class="flex flex-col gap-y-2">
-        <UFormField label="email" name="email" size="lg" required>
-            <UInput type="email" />
-        </UFormField>
-        <UFormField label="password" name="password" size="lg" required>
-            <UInput type="password" />
-        </UFormField>
-        <UButton type="submit" class="inline-flex cursor-pointer justify-center"
-            >Login</UButton
-        >
-    </UForm>
-</template>
+<script setup lang="ts">
+import redirect from '@/middleware/redirect';
+
+definePageMeta({
+    middleware: redirect,
+});
+</script>
---
+++ app/pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+    <UForm class="flex flex-col gap-y-2">
+        <UFormField label="email" name="email" size="lg" required>
+            <UInput type="email" />
+        </UFormField>
+        <UFormField label="password" name="password" size="lg" required>
+            <UInput type="password" />
+        </UFormField>
+        <UButton type="submit" class="inline-flex cursor-pointer justify-center"
+            >Login</UButton
+        >
+    </UForm>
+</template>
