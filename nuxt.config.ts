d80efc09e89c7da471f419588d4af548aef088fc
lorypelli <lorypelli@proton.me>
2026-01-11 12:38:56 +0100

    use nuxt hub for the db

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-    compatibilityDate: '2026-01-05',
-    modules: ['@nuxt/ui'],
+    compatibilityDate: '2026-01-10',
+    modules: ['@nuxt/ui', '@nuxthub/core'],
     vite: {
         plugins: [tailwindcss()],
+    },
+    hub: {
+        db: 'sqlite',
     },
 });
