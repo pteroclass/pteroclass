495ecbe18a0140978cc07be52d41231a302cdc99
lorypelli <lorypelli@proton.me>
2026-01-12 07:10:29 +0100

    added type helper

--- app/db/user.ts
+++ app/db/user.ts
@@ -1,9 +1,11 @@
-import { mysqlTable, varchar, boolean } from 'drizzle-orm/mysql-core';
+import { boolean, mysqlTable, varchar } from 'drizzle-orm/mysql-core';
 
-export const userTable = mysqlTable('users', {
+export const users = mysqlTable('users', {
     id: varchar('id', { length: 36 }).primaryKey().default(crypto.randomUUID()),
     email: varchar('email', { length: 255 }).notNull().unique(),
     password: varchar('password', { length: 60 }).notNull(),
     role: varchar('role', { length: 7 }).notNull().default('student'),
     is_verified: boolean('is_verified').notNull().default(false),
 });
+
+export type User = typeof users.$inferInsert;
