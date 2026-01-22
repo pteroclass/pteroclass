a986bc1def2b4c31d3115d7c21924e1592af0c77
lorypelli <lorypelli@proton.me>
2026-01-22 20:50:04 +0100

    comunicate if user needs verification

--- server/api/admin/register.post.ts
+++ server/api/admin/register.post.ts
@@ -2,33 +2,32 @@ import bcrypt from 'bcrypt';
 import z from 'zod';
 
 export default defineEventHandler(async (e) => {
-    const success = await $fetch('/api/admin/exists');
+    const { success } = await $fetch('/api/admin/exists');
     if (success) {
         throw createError({
             statusCode: 403,
             statusMessage: 'Admin already exists',
         });
     }
-    const body = await readBody(e);
-    const parsedBody = adminZodSchema.safeParse(body);
-    if (!parsedBody.success) {
+    const body = await readValidatedBody(e, adminZodSchema.safeParse);
+    if (!body.success) {
         throw createError({
             statusCode: 400,
             statusMessage: 'Invalid request body',
-            data: z.treeifyError(parsedBody.error),
+            data: z.treeifyError(body.error),
         });
     }
-    const { email, password, role, verified } = parsedBody.data;
+    const { email, password, role, verified } = body.data;
     const hashedPassword = await bcrypt.hash(password, 12);
-    const is_verified = verified == 'Yes';
+    const isVerified = verified == 'Yes';
     await db.insert(schema.users).values({
         email,
         password: hashedPassword,
         role,
-        is_verified,
+        isVerified,
     });
-    if (!is_verified) {
-        return { message: 'You need to change your password', success: false };
+    if (!isVerified) {
+        return { message: 'You need to change your password', needsVerification: true, success: false };
     }
-    return { message: 'Admin registered successfully', success: true };
+    return { message: 'Admin registered successfully', needsVerification: false, success: true };
 });
